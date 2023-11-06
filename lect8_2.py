515b92cc0eaf5a87faa74b30b432b15f0084a1d1
Cho minseo <jms529411@gmail.com>
2023-11-06 16:29:13 +0900

    first commit

--- lect8_2.py
+++ lect8_2.py
@@ -1,7 +1,25 @@
 # 입력 암호화
+"""
 import getpass
 
 pw = input("Pass :")
 print(pw)
 pw = getpass.getpass("Pass : ")
 print(pw)
+"""
+"""
+import hashlib
+
+hl = hashlib.sha256()
+target = "hello"
+
+hl.update(target.encode("utf-8"))
+"""
+import Crypto.Hash.keccak as kek
+
+target = "hello"
+
+kksh = kek.new(digest_bits=256)
+kksh.update(target.encode("utf-8"))
+
+print(kksh.hexdigest())
